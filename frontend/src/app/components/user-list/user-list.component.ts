d2cb576aaf5e7fdccef1bc12ea7184fcf86217a3
EC2_BUILD <adam.stypczyc@gmail.com>
2024-11-16 17:01:09 +0100

    Lost of users v1

---
+++ frontend/src/app/components/user-list/user-list.component.ts
@@ -0,0 +1,29 @@
+import { Component } from '@angular/core';
+import {User} from "../../models/user";
+import {AuthService} from "../../services/authService";
+@Component({
+  selector: 'app-user-list',
+  standalone: true,
+  imports: [],
+  templateUrl: './user-list.component.html',
+  styleUrl: './user-list.component.scss'
+})
+export class UserListComponent {
+  users: User[] = [];
+  constructor(private authService: AuthService) {
+  }
+  ngOnInit():void{
+    this.loadUsers();
+  }
+
+  loadUsers() {
+    this.authService.getAllUsers().subscribe(
+        (users: User[]) => {
+          this.users = users;
+        },
+        (error) => {
+          console.error("Błąd podczas pobierania użytkowników", error);
+        }
+    )
+  }
+}
