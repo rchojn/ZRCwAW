2ac56f6c06fe3addfc881a6d9f1d8078415aa5e6
EC2_BUILD <adam.stypczyc@gmail.com>
2024-11-16 17:48:16 +0100

    List of users v4

--- frontend/src/app/components/user-list/user-list.component.ts
+++ frontend/src/app/components/user-list/user-list.component.ts
@@ -1,11 +1,12 @@
 import { Component, OnInit,  } from '@angular/core';
 import {User} from "../../models/user";
 import {AuthService} from "../../services/authService";
+import {CommonModule} from "@angular/common";
 
 @Component({
   selector: 'app-user-list',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './user-list.component.html',
   styleUrl: './user-list.component.scss'
 })
@@ -18,13 +19,13 @@ export class UserListComponent {
   }
 
   loadUsers() {
-    this.authService.getAllUsers().subscribe(
-        (users: User[]) => {
-          this.users = users;
-        },
-        (error) => {
-          console.error("Błąd podczas pobierania użytkowników", error);
-        }
-    )
+    this.authService.getAllUsers().subscribe({
+      next: (users: User[]) => {
+        this.users = users;
+      },
+      error: (error) => {
+        console.error("Błąd podczas pobierania użytkowników", error);
+      }
+    });
   }
 }
