4c7aac125b0403671779ff0cbdfe48ae96ca04aa
EC2_BUILD <adam.stypczyc@gmail.com>
2024-11-18 08:39:59 +0100

    Cognito v2

--- backend/src/main/java/com/pwr/project/services/AuthService.java
+++ backend/src/main/java/com/pwr/project/services/AuthService.java
@@ -1,11 +1,16 @@
 package com.pwr.project.services;
 
+import com.amazonaws.services.cognitoidp.AWSCognitoIdentityProvider;
+import com.amazonaws.services.cognitoidp.model.*;
+import com.pwr.project.dto.JwtDTO;
+import com.pwr.project.dto.LoginDTO;
 import com.pwr.project.dto.RegisterDTO;
 import com.pwr.project.entities.User;
 import com.pwr.project.exceptions.InvalidJWTException;
 import com.pwr.project.repositories.UserRepository;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
@@ -13,6 +18,7 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Map;
 
 @Service
 public class AuthService implements UserDetailsService {
@@ -20,17 +26,57 @@ public class AuthService implements UserDetailsService {
     @Autowired
     UserRepository userRepository;
 
+    @Autowired
+    private AWSCognitoIdentityProvider cognitoClient;
+
+    @Value("${aws.cognito.userPoolId}")
+    private String userPoolId;
+
+    @Value("${aws.cognito.clientId}")
+    private String clientId;
+
+
     @Override
     public UserDetails loadUserByUsername(String login){
         return userRepository.findByLogin(login);
     }
     public void register(RegisterDTO registerDTO) throws InvalidJWTException {
-        if (userRepository.findByLogin(registerDTO.login()) != null){
-            throw new InvalidJWTException("Username already exists");
+        try {
+            AdminCreateUserRequest request = new AdminCreateUserRequest()
+                    .withUserPoolId(userPoolId)
+                    .withUsername(registerDTO.login())
+                    .withUserAttributes(
+                            new AttributeType().withName("email").withValue(registerDTO.login()),
+                            new AttributeType().withName("given_name").withValue(registerDTO.firstname()),
+                            new AttributeType().withName("family_name").withValue(registerDTO.surname()),
+                            new AttributeType().withName("custom:isSeller").withValue(registerDTO.isSeller().toString())
+                    )
+                    .withTemporaryPassword(registerDTO.password());
+
+            cognitoClient.adminCreateUser(request);
+
+        } catch (AWSCognitoIdentityProviderException e) {
+            throw new InvalidJWTException("Error while creating user in Cognito " + e.getMessage());
         }
-        String encryptedPassword = new BCryptPasswordEncoder().encode(registerDTO.password());
-        User newUser = new User(registerDTO.firstname(), registerDTO.surname(),registerDTO.login(), encryptedPassword, registerDTO.isSeller());
-        userRepository.save(newUser);
+    }
+
+    public JwtDTO login(LoginDTO loginDTO) {
+        try {
+            AdminInitiateAuthRequest authRequest = new AdminInitiateAuthRequest()
+                    .withAuthFlow(AuthFlowType.ADMIN_NO_SRP_AUTH)
+                    .withUserPoolId(userPoolId)
+                    .withClientId(clientId)
+                    .withAuthParameters(Map.of(
+                            "USERNAME", loginDTO.login(),
+                            "PASSWORD", loginDTO.password()
+                    ));
+
+            AdminInitiateAuthResult result = cognitoClient.adminInitiateAuth(authRequest);
+            return new JwtDTO(result.getAuthenticationResult().getIdToken());
+
+        } catch (AWSCognitoIdentityProviderException e) {
+            throw new InvalidJWTException("Error while creating user in Cognito " + e.getMessage());
+        }
     }
 
     public User getCurrentUser() {
