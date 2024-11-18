7398599f6dc955d699f01b55fe5cf232c458ed7e
EC2_BUILD <adam.stypczyc@gmail.com>
2024-11-18 09:16:10 +0100

    Cognito frontend v1

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -1,6 +1,16 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
+import {Amplify} from "aws-amplify";
+
+Amplify.configure({
+  Auth: {
+    Cognito: {
+      userPoolId: 'us-east-1_OMPIqfVaW',
+      userPoolClientId: '59idbtnj82biv9ii6n6n8rhb2t'
+    }
+  }
+});
 
 bootstrapApplication(AppComponent, appConfig)
   .catch((err) => console.error(err));
