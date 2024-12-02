36c535f27dfc25da3d106bcb945a7b699df97099
rchojn <rafalchojnacki0502@gmail.com>
2024-12-02 16:06:16 +0100

    new ignore

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -1,15 +1,16 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
-import {Amplify} from "aws-amplify";
+import { Amplify } from '@aws-amplify/core';
 
 Amplify.configure({
   Auth: {
     Cognito: {
-      userPoolId: 'us-east-1_OMPIqfVaW',
-      userPoolClientId: '59idbtnj82biv9ii6n6n8rhb2t'
-    }
+    userPoolId: 'us-east-1_OMPIqfVaW',
+    userPoolClientId: '59idbtnj82biv9ii6n6n8rhb2t',
   }
+}
 });
+
 bootstrapApplication(AppComponent, appConfig)
   .catch((err) => console.error(err));
